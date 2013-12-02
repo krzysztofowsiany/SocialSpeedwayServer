b9408f55efa869dbe9718f855c625d1bdaa7d2ff
Gocom <gocom@debian>
2013-12-02 22:36:23 +0100

    add databasestruct model, get playerid after login

--- Player.js
+++ Player.js
@@ -25,9 +25,15 @@ exports.register = function (data, gameSocket) {
  * @param gameSocket
  */
 exports.login = function (data, gameSocket) {
-	db.queryResults('SELECT * FROM players WHERE name=$1 AND password=$2;', [data['name'], data['password']],
+	console.log(data);
+	db.queryResults('SELECT playerid FROM players WHERE name=$1 AND password=$2;', [data['name'], data['password']],
 			function (results)	{
-					gameSocket.emit('login_result', {login_result:results.length});
+				if (results.length==1) {
+					gameSocket.emit('login_result', {login_result:results[0].playerid});
+					console.log(results[0]);
+				}
+				else
+					gameSocket.emit('login_result', {login_result:0});
 				
 			});	
 };
@@ -42,6 +48,34 @@ exports.save_profile = function (data, gameSocket) {
 	//db.queryResults('SELECT * FROM players WHERE name=$1 AND password=$2;', [data['name'], data['password']],
 		//	function (results)	{
 			//		gameSocket.emit('profile_result', {login_result:results.length});
+				
+			//});	
+};
+
+/**
+ * save contact
+ * @param data
+ * @param gameSocket
+ */
+exports.save_contact = function (data, gameSocket) {
+	console.log(data);
+	//db.queryResults('SELECT * FROM players WHERE name=$1 AND password=$2;', [data['name'], data['password']],
+		//	function (results)	{
+			//		gameSocket.emit('profile_result', {login_result:results.length});
+				
+			//});	
+};
+
+/**
+ * getData
+ * @param data
+ * @param gameSocket
+ */
+exports.getData = function (data, gameSocket) {
+	console.log(data);
+	//db.queryResults('SELECT * FROM players WHERE name=$1 AND password=$2;', [data['name'], data['password']],
+		//	function (results)	{
+			//		gameSocket.emit('resultData', {login_result:results.length});
 				
 			//});	
 };
